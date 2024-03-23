a65e80c29b0d3c2af000d69e1c143d513b507315
Macaicedoa <117154985+Macaicedoa@users.noreply.github.com>
2024-03-23 12:31:52 +0100

    Ejercicio imagen

--- Ejercicio011/src/server.ts
+++ Ejercicio011/src/server.ts
@@ -2,8 +2,18 @@ import express from "express";
 import "dotenv/config"
 import "express-async-errors"
 import morgan from "morgan";
-import { getAll, getOneByID,create,updateById,deleteByID } from "./controllers/planets.js"
+import { getAll, getOneByID,create,updateById,deleteByID,createImage } from "./controllers/planets.js"
+import multer from "multer";
 
+const storage = multer.diskStorage({
+   destination: (req,file,cb)=>{
+      cb(null,"./uploads")
+   },
+   filename:(req,file,cb)=>{
+      cb(null,file.originalname)
+   }
+})
+const upload = multer({storage})
 const app = express()
 const port = process.env.PORT;
 
@@ -20,6 +30,8 @@ app.post('/api/planets',create)
 app.put('/api/planets/:id',updateById)
 
 app.delete('/api/planets/:id',deleteByID)
+
+app.post('/api/planets/:id/image', upload.single("image") ,createImage)
 
 app.listen(port,()=>{
    console.log(`http://localhost:${port}`) 
