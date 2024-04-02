ed2171c6b2bce26ec16b09a30e368d6358a0f581
Macaicedoa <117154985+Macaicedoa@users.noreply.github.com>
2024-04-02 13:21:33 +0200

    Ex009

--- Ejercicio006/luckyDraw.js
+++ Ejercicio006/luckyDraw.js
@@ -13,14 +13,27 @@ function luckyDraw(player) {
   }
 
 
-const players = ['Joe', 'Caroline', 'Sabrina'];
+// const players = ['Joe', 'Caroline', 'Sabrina'];
 
-players.forEach(player=>{
-    luckyDraw(player).then(val=>{
-        console.log(val)
-    }).catch(err=>{
-        console.log(err.message)
-    })
-})
+// players.forEach(player=>{
+//     luckyDraw(player).then(val=>{
+//         console.log(val)
+//     }).catch(err=>{
+//         console.log(err.message)
+//     })
+// })
 
 
+async function getResults() {
+    const players = ['Tina', 'Jorge', 'Julien'];
+  
+    for (let i=0;i<players.length;i++) {    //No puedo usar un foreach porque el await requiere de una funcion async 
+      try {
+        const result = await luckyDraw(players[i]);
+        console.log(result);
+      } catch (error) {
+        console.error(error.message);
+      }
+    }
+  }
+  getResults();
