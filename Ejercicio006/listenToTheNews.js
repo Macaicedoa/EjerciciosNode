33069c1b0af9bbb96a2b30972b8eb16153845791
Macaicedoa <117154985+Macaicedoa@users.noreply.github.com>
2024-04-02 13:36:51 +0200

    ultimo

---
+++ Ejercicio006/listenToTheNews.js
@@ -0,0 +1,33 @@
+import { EventEmitter } from "node:events";
+
+function createNewsFeed() {
+  const emitter = new EventEmitter();
+
+  emitter.on("newsEvent",(data)=>{
+    console.log(data)
+  });
+
+  setInterval(() => {
+    emitter.emit("newsEvent", "News: A thing happened in a place.");
+  }, 1000);
+
+  emitter.on("breakingNews",(data)=>{
+    console.log(data)
+  });
+
+  setInterval(() => {
+    emitter.emit("breakingNews", "Breaking news! A BIG thing happened.");
+  }, 4000);
+
+  emitter.on("error",(error)=>{
+    console.log(error.message)
+  });
+
+  setTimeout(() => {
+    emitter.emit("error", new Error("News feed connection error"));
+  }, 5000);
+
+  return emitter;
+}
+
+const newsFeed = createNewsFeed();
